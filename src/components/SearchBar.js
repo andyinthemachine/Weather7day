8ec7d5a16141c404ac38d5d1e890fefa3b741a15
Andrew Ulrich <andyinthemachine@hotmail.com>
2019-08-21 10:21:01 -0600

    city form update fix

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,16 +3,16 @@ import { Form, FormGroup, Input, Button } from 'reactstrap';
 
 const SearchBar = props => {
     return (<>
-        <Form>
-            {/* <FormGroup> */}
-            <Input
-                onChange={props.handleInputChange}
-                value={props.value} type="text"
-                name={props.name} placeholder="City, State/Country" />
-            <Button
-                onClick={props.handleFormSubmit}>Submit
+        <Form onSubmit={props.handleFormSubmit}>
+            <FormGroup>
+                <Input
+                    onChange={props.handleInputChange}
+                    value={props.value} type="text"
+                    name={props.name} placeholder="City, State/Country" />
+                <Button
+                    onClick={props.handleFormSubmit}>Submit
                 </Button>
-            {/* </FormGroup> */}
+            </FormGroup>
         </Form>
     </>
     )
